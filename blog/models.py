cb7370008d5278305654c46260b88d88ac9068dd
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-18 15:45:20 +0900

    게시물에 대한 SEO 친화적인 URL 생성

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -35,8 +35,10 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
-
     def __str__(self):
         return self.title
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
