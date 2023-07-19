6019c8e582387d2546f1408e83945b5e0454f374
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-19 13:18:00 +0900

    함수 기반 뷰(post_list)를 클래스 기반 뷰(PostListView)로 변경

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
             views.post_detail, name='post_detail')
 ]
