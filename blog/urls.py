e561b59809dcb5de2a3c651b15106224b0bc50b0
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-20 09:19:53 +0900

    메일 공유 기능 개발

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
-            views.post_detail, name='post_detail')
+            views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
